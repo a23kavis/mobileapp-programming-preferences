46fabaf6a520e0672f4d2f0b4988719bbe33a475
a23kavis <a23kavis@student.his.se>
2024-10-27 03:10:29 +0100

    Add editor & button to send data (closes with finish())

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,10 +1,12 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -13,6 +15,23 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
+        final EditText dataEditText = findViewById(R.id.dataEditText);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String dataToStore = dataEditText.getText().toString();
+
+                SharedPreferences sharedPreferences = getSharedPreferences("pref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("key", dataToStore);
+                editor.apply();
+
+                finish();
+            }
+        });
     }
 }
